30c794c06ac2632846d166f8a9e213cf38ffb266
Sam Johnson <s_johnso@yahoo.com>
2023-07-06 14:37:53 -0500

    Added search function to js file

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,23 +3,23 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 // Function to add records
-let album = {};
+
 function addToCollection(title, artist, yearPublished) {
-    album = {
-        title: title,
-        artist: artist,
-        yearPublished: yearPublished,
-    };
-    collection.push(album); 
-    return album;
+    collection.push({title, artist, yearPublished});
+    return collection[collection.length - 1];
 }
 // End addToCollection
 
-addToCollection('Car Wheels on a Gravel Road', 'Lucinda Williams', 1998); // adds album to collection.
-console.log(album); // logs added album.
+console.log(addToCollection('Car Wheels on a Gravel Road', 'Lucinda Williams', 1998)); // adds album to collection.
+// logs added album.
 
+// Log the full collection
+console.log('This is the collection: ', collection);
+
+// Function to list collection in sentence form.
 function showCollection(array) {
-    console.log(array.length);
+    console.log('This is the number of albums in this collection: ', array.length);
+    console.log('Here are the albums in this collection, listed with showCollection:')
     for (let each of array) {
         console.log(`${each.title} by ${each.artist}, published in ${each.yearPublished}`);
     }
@@ -27,9 +27,12 @@ function showCollection(array) {
 // End showCollection.
 
 showCollection(collection);
-
-let artistCollection = [];
+ 
+// Find all albums by one artist
 function findByArtist(artist) {
+    
+    let artistCollection = [];
+    
     for (let each of collection) { //Cycling through to match to albums.
         if (artist === each.artist) { 
             artistCollection.push(each); //If input matches entry artist, add entry to artistCollection.    
@@ -39,6 +42,28 @@ function findByArtist(artist) {
 }
 // End findByArtist
 
-findByArtist('Lucinda Williams');
-console.log(artistCollection);
+// Test findByArtist
+console.log('Lucinda Williams records in my collection: ', findByArtist('Lucinda Williams'));
 
+// Album search
+
+function search({artist, yearPublished}) {
+
+    let searchResult = [];
+
+    console.log(`in search: ${artist} ${yearPublished}`)
+    if (artist && yearPublished) {
+        for (let each of collection) {
+            if (each.artist == artist && each.yearPublished == yearPublished) {
+                searchResult.push(each);
+            } 
+        }
+    } else {
+        return collection
+    }
+    return searchResult;
+};
+// End search
+
+let searchResult = search({ yearPublished: 1998});
+console.log('Should be an array: ', searchResult);
